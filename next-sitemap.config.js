4696b28ba39315ca36540f61136ed936d4cd818d
Anurag Volety <anurag@volety.dev>
2024-02-23 19:41:21 +0530

    seo init

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    siteUrl: 'https://volety.dev', // Your website URL
+    generateRobotsTxt: true, // Generate a robots.txt file
+    // The default sitemap size (5000 URLs per sitemap file) is used here
+    outDir: './public', // Output directory for the sitemap files is set to 'public'
+    // No specific paths are excluded since you indicated that all pages should be indexed
+    // No additional sitemaps or URL transformations are needed as per your responses
+  };
+  
